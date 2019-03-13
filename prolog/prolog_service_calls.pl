cd3f37992c95a19bdfc20561b62e99f453227390
Björn Veit <bveit@uni-bremen.de>
2019-03-13 09:54:10 +0100

    clean up and last changes

--- prolog/prolog_service_calls.pl
+++ prolog/prolog_service_calls.pl
@@ -1,11 +1,12 @@
 :- module(prolog_service_calls,
     [
-    spawn_model/2,
+    spawn_models/2,
     spawn_constraint/1,
     attach_model_to_parent/2,
-    spawn_semantic_map/4
+    spawn_semantic_map/4,
+    spawn_semantic_map/1
     ]).
 
 :- use_foreign_library('libprolog_service_calls.so').
-:- consult('./spawn_semantic_map.pl').
+:- consult('./spawn_semantic_map_one_service_call.pl').
 :- ros_init.
